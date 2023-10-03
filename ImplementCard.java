14da670b6af0e9c4fdb545e79ffdc3837f00a988
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-02 20:47:21 -0400

    initial upload of the ImplementCard class

---
+++ ImplementCard.java
@@ -0,0 +1,25 @@
+package GoFishProject;
+public abstract class ImplementCard implements Card {
+    private String suit;
+    private String rank;
+
+    public ImplementCard(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank(){
+        return this.rank;
+    }
+
+    public String getSuit(){
+        return this.suit;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(rank + " of " + suit);
+        return sb.toString();
+    }
+
+}
