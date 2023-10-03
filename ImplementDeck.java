c20a745fe6791c761fac2f01aeedd75c6aa504f6
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-02 22:46:19 -0400

    initial upload of file that implements Deck class

---
+++ ImplementDeck.java
@@ -0,0 +1,35 @@
+package GoFishProject;
+import java.util.*;
+
+public class ImplementDeck implements Deck{
+    private List<Card> cards;
+
+    public ImplementDeck(){
+        initializeDeck();
+    }
+
+    private void initializeDeck(){
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+        for (String suit : suits) {
+            for (String rank : ranks){
+                cards.add(new ImplementCard(rank, suit));
+            }
+        }
+    }
+
+    @Override
+    public Card drawCard(){
+        if (!cards.isEmpty()){
+            return cards.remove(0);
+        }
+        return null;
+    }
+
+    @Override
+    public void shuffleDeck(){
+        Collections.shuffle(cards);
+    }
+}
