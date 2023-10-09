748bbd2aee059a15095fc12c4522ecf40f04cec3
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-09 17:53:32 -0400

    started on the startGame method and added to the isGameOver method

--- ImplementGame.java
+++ ImplementGame.java
@@ -15,14 +15,34 @@ public class ImplementGame implements Game{
     public void startGame(){
         System.out.println("Let's begin playing Go Fish!");
 
+        //hand out number cards to each player based on number players
+        if (players.size() == 2 || players.size() == 3) {
+            for (Player player : players) {
+                for (int i = 1; i <=7; i++) {
+                    player.addToHand(deck.drawCard());
+                }
+            }
+        } else if (players.size() > 3) {
+            for (Player player : players) {
+                for (int i = 1; i <=5; i++) {
+                    player.addToHand(deck.drawCard());
+                }
+            }
+        }
+
+        //start the players turn
+        while (!isGameOver()){
+            players.get(0).play();
+        }
     }
 
     @Override
     public boolean isGameOver(){
+        //the game is over if the deck finishes or if a player finishes his cards
         if (deck.isEmpty()){
             return true;
-        } else if () {
-
+        } else if (players.get(0).getHand() == null) {
+            return true;
         } else {
             return false;
         }
