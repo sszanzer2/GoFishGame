0770fb56f6c12c8a04d0fa12152817dc2c6bc451
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-11 22:56:15 -0400

    Moved the old code to the correct file

--- GoFishGame.java
+++ GoFishGame.java
@@ -13,12 +13,43 @@ public class GoFishGame implements Game {
     @Override
     public void startGame() {
         // Implement the game initialization here
+        System.out.println("Let's begin playing Go Fish!");
+
+        //shuffle the deck
+        deck.shuffleDeck();
+
+        //hand out 7 cards to each player
+        for (Player player : players) {
+            for (int i = 1; i <=7; i++) {
+                Card cardToAdd = deck.drawCard(); // Draw a card from the deck. This method also removes it from deck
+                player.addToHand(cardToAdd); // Add the card to the player's hand
+            }
+        }
+
+        //start the players turn
+        while (!isGameOver()){
+            for (Player player : players) {
+                if(!isGameOver()){
+                    playTurn(player);
+                    isGameOver();
+                }
+            }
+        }
+        endGame();
     }
+
 
     @Override
     public boolean isGameOver() {
         // Implement the game-over condition here
-        return false; // Replace with your condition
+        //the game is over if the deck finishes or if a player finishes his cards
+        if (deck.isEmpty()){
+            return true;
+        } else if (players.get(0).getHand() == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
