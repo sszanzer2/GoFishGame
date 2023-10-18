5bb04c8325cdacdfbdd96baa6a87fa94c9cd82cf
Shoshana Szanzer <sszanzer2@student.touro.edu>
2023-10-18 12:49:02 -0400

    Added to main

--- Main.java
+++ Main.java
@@ -1,24 +1,26 @@
 package GoFishProject;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		    // Create and initialize your players
-		    List<Player> players = new ArrayList<>();
-		    players.add(new HumanPlayer("Player 1", new ArrayList<Card>()));
-		    players.add(new ComputerPlayer("Computer", new ArrayList<Card>()));
-		    
-		    // Create and initialize your deck
-		    Deck deck = new ImplementDeck();
-
-		    // Create the game and start it
-		    GoFishGame game = new GoFishGame(players, deck);
-		    game.startGame();
-		
-	}
-
-
+    public static void main(String[] args) {
+        // Create the deck and players
+        Deck deck = new ImplementDeck();
+        List<Player> players = new ArrayList<>();
+        
+        // Create human and computer players
+        List<Card> humanHand = new ArrayList<>();
+        List<Card> computerHand = new ArrayList<>();
+        
+        Player humanPlayer = new HumanPlayer("Human", humanHand);
+        Player computerPlayer = new ComputerPlayer("Computer", computerHand);
+        
+        players.add(humanPlayer);
+        players.add(computerPlayer);
+        
+        // Create and start the game
+        GoFishGame game = new GoFishGame(players, deck);
+        game.startGame();
+    }
 }
